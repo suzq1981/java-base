900fb2abee588e52b2811a9286d7ea1b4d9fdc60
suzq1981 <97387587+suzq1981@users.noreply.github.com>
2022-04-24 17:03:33 +0800

    Update EmpController.java

--- mybatis/src/main/java/com/djt/mapper/controller/EmpController.java
+++ mybatis/src/main/java/com/djt/mapper/controller/EmpController.java
@@ -14,13 +14,15 @@ public class EmpController {
 
 	@Autowired
 	private IEmployeeService employeeService;
-
+        // 1、添加指定的员工信息
+	// 2、返回添加的数量
 	@RequestMapping("/crt")
 	public int create() {
 		Employee emp = Employee.builder().empName("冯存钱").gender('M').address("浙江省绍兴市嘉善县").dept(Department.builder().deptId(2).build()).build();
 		return employeeService.createEmp(emp);
 	}
-
+        // 1、删除指定姓名的员工
+	// 2、返回删除的行数
 	@RequestMapping("/del")
 	public int delete() {
 		System.out.println("DDDDDDDDDDDDD");
